6e236f82371f3626bd350121fe42cec83d27ea82
Yilun Zhang <yiz202@ucsd.edu>
2016-07-04 10:01:48 +0800

    length of last word

---
+++ 58.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        if s == "": return 0
+
+        for i in range(len(s)-1,-1,-1):
+            if s[i] == " ": return len(s[i+1:])
+        return len(s)
+        
