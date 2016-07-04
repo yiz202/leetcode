81418f8092ff7b15239a5b766e66d9abd8f5880a
Yilun Zhang <yiz202@ucsd.edu>
2016-07-04 16:44:13 +0800

    move zeros

---
+++ 283.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+        for i in range(len(nums)):
+            if nums[i]!= 0:
+                nums[pos] = nums[i]
+                pos+=1
+        for i in range(pos,len(nums)):
+            nums[i] = 0
+
+
+
+        
